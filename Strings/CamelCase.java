00c62900782d7e2e5a124079035b724f1ed5fd39
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-08-14 01:05:46 -0500

    Camel Case Simple Problem

--- Strings/CamelCase.java
+++ Strings/CamelCase.java
@@ -33,11 +33,27 @@ The
 Editor
 Thus, we print  on a new line.
  */
-public class CamelCase {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	import java.io.*;
+	import java.util.*;
+	import java.text.*;
+	import java.math.*;
+	import java.util.regex.*;
 
+	public class CamelCase {
+
+	    public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        String s = in.next();
+	        
+	        int count = 1;  //counts the number of words
+	        for(int i=0; i< s.length(); i++)
+	        {
+	            char c = s.charAt(i);
+	            if(Character.isUpperCase(c))
+	                count++;
+	        }
+	        
+	        System.out.println(count);
+	    }
 	}
-
-}
