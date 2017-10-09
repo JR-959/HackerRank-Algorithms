969e18f4654a4bed052d543bc0fcba19f5915bc1
Jakhongir Rikhsiboev <j.rikhsiboev@gmail.com>
2017-10-09 17:36:11 -0500

    Fixed identation

--- Arrays/Arrays_DS.java
+++ Arrays/Arrays_DS.java
@@ -30,33 +30,26 @@ Sample Output
 2 3 4 1
  */
 
+import java.io.*;
+import java.util.*;
 
-
-	import java.io.*;
-	import java.util.*;
-
-	public class Arrays_DS {
-
-	    public static void main(String[] args) {
-	        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-	        Scanner scan = new Scanner(System.in);
-	        int numArg = scan.nextInt();
-	        int arr[] = new int[numArg];
+public class Arrays_DS {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+	    int numArg = scan.nextInt();
+	    int arr[] = new int[numArg];
 	        
-	        //Fills out the array
-	        for(int i=0; i< numArg; i++)
-	        {
-	            arr[i] = scan.nextInt();
-	        }
-	        
-	        //Outputs the arrays in reverse order
-	        for(int i=arr.length-1; i >= 0; i--)
-	        {
-	            System.out.print(arr[i] + " ");
-	        }
-	        
-	        
-	        
-	        
+	    //Fills out the array
+	    for(int i=0; i< numArg; i++)
+	    {
+	    		arr[i] = scan.nextInt();
 	    }
-	}
+	        
+	    //Outputs the arrays in reverse order
+	    for(int i=arr.length-1; i >= 0; i--)
+	    {
+	       System.out.print(arr[i] + " ");
+	    }//end of for loop
+	    
+	}// end of main
+}
